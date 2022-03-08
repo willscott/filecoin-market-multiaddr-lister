d3169d0b3d7a57577ed4ce9c8e4716fa8df21e39
Will Scott <will.scott@protocol.ai>
2022-03-08 10:29:04 +0100

    push latest

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ import { NodejsProvider } from '@filecoin-shipyard/lotus-client-provider-nodejs'
 import { mainnet } from '@filecoin-shipyard/lotus-client-schema'
 import { multiaddr } from 'multiaddr'
 import fetch from 'node-fetch'
-
+import { BufferList } from 'bl'
+import { encode, Token, Type } from 'cborg'
+import { Duplex } from 'stream'
+import { CID } from 'multiformats/cid'
+import { sha256 } from 'multiformats/hashes/sha2'
 
 const url = 'https://api.node.glif.io/rpc/v0'
 const provider = new NodejsProvider(url)
@@ -19,31 +23,50 @@ function _base64ToArrayBuffer(base64) {
     return bytes;
 }
 
-async function getInfoFor(miner) {
+async function getInfoFor(miner, pid) {
     const info = await client.stateMinerInfo(miner, [])
     if (info.PeerId != null) {
+        if (pid != undefined && pid != info.PeerId) {
+            return
+        }
         if (info.Multiaddrs.length > 0) {
             for (const addr of info.Multiaddrs) {
                 const bytes = _base64ToArrayBuffer(addr)
                 const ma = multiaddr(bytes)
+                console.log(miner)
                 console.log(ma.encapsulate('/p2p/' + info.PeerId).toString())
 
-                const data = {
-                    Cid: '',
-                    Addrs: [ma.encapsulate('/p2p/' + info.PeerId).toString()]
-                };
-
-                const url = 'http://abcca6d4490f0426d9dd855139563762-159596440.us-east-2.elb.amazonaws.com:3001/ingest/announce';
                 try {
+                    const mab = ma.encapsulate('/p2p/' + info.PeerId).bytes
+                    const none = Buffer.alloc(0)
+                    const advertisementCid = CID.create(1, 0x0129, await sha256.digest(none))
+               const cbor = encode([advertisementCid, [mab], Buffer.alloc(0)],
+               {
+                typeEncoders: {
+                  Object: function (cid) {
+                    // CID must be prepended with 0 for historical reason - See: https://github.com/ipld/cid-cbor
+                    const bytes = new BufferList(Buffer.alloc(1))
+                    bytes.append(cid.bytes)
+      
+                    return [new Token(Type.tag, 42), new Token(Type.bytes, bytes.slice())]
+                  }
+                }
+              })
+
+                //const url = 'http://abcca6d4490f0426d9dd855139563762-159596440.us-east-2.elb.amazonaws.com:3001/ingest/announce';
+                const url = 'http://18.116.210.184:3001/ingest/announce';
+                    let stream = new Duplex();
+                    stream.push(cbor);
+                    stream.push(null);
+
                     const response = await fetch(url, {
                         method: 'PUT',
                         mode: 'same-origin',
                         headers: {
-                            'Content-Type': 'application/json'
+                            'Content-Type': 'application/cbor'
                         },
-                        body: JSON.stringify(data)
+                        body: stream
                     })
-                    console.log(response.status)
                 } catch(e) {
                     console.log(e)
                 }
@@ -54,14 +77,14 @@ async function getInfoFor(miner) {
     return null
 }
 
-async function run () {
+async function run (pid) {
   try {
 
     const markets = await client.stateMarketParticipants([])
 
     for (const miner of Object.keys(markets)) {
         try {
-            const mi = await getInfoFor(miner)
+            const mi = await getInfoFor(miner, pid)
         } catch (e) {
             continue
         }
@@ -71,4 +94,9 @@ async function run () {
   }
   await client.destroy()
 }
-run()
+
+if (process.argv.length > 2) {
+    run(process.argv[2])
+} else {
+    run()
+}
